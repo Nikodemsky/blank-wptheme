4b6ea863c3c6ef71eed3c801bb1a19f6bab8c152
Nikodemsky <Nikodemh@gmail.com>
2025-09-14 23:17:51 +0200

    Burger-menu handler

---
+++ assets/js/menu-toggle.js
@@ -0,0 +1,17 @@
+// Get the required elements
+/*const menuBtn = document.querySelector('.menu-btn');
+const closedSpan = menuBtn.querySelector('.closed');
+const openedSpan = menuBtn.querySelector('.opened');
+const navigation = document.querySelector('#slide-menu');
+
+// Add click event listener to the button
+menuBtn.addEventListener('click', toggleClasses);
+
+function toggleClasses() {
+  // Toggle classes for the button spans
+  closedSpan.classList.toggle('hidden');
+  openedSpan.classList.toggle('hidden');
+
+  // Toggle the 'visible' class for the navigation
+  navigation.classList.toggle('visible');
+}*/
