51d2f7efa0afa065bcb17d326be958b0a92f7cfc
Nikodemsky <Nikodemh@gmail.com>
2025-09-15 22:01:34 +0200

    schema update

--- header.php
+++ header.php
@@ -9,64 +9,13 @@
 	<?php wp_head(); ?>
 
 	<!-- Zapis schema dla wpisow blogowych / Schema markup for posts -->
-	<?php if (is_singular('post') && function_exists('tsf')) : ?>
-	<script type="application/ld+json">
-	{
-	"@context": "https://schema.org",
-	"@type": "Article",
-	"headline": "<?php echo single_post_title(); ?>",
-	"description": "<?php echo the_seo_framework()->get_description(); ?>",
-	"image": "<?php echo get_the_post_thumbnail_url( '', 'full' ); ?>",  
-	"author": {
-		"@type": "Organization",
-		"name": " Business / Author name",
-		"url": "https://website-url.com"
-	},  
-	"publisher": {
-		"@type": "Organization",
-		"name": "Business",
-		"logo": {
-		"@type": "ImageObject",
-		"url": "<?php if (has_custom_logo()) : echo esc_url( wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' )[0] ); endif; ?>"
-		}
-	},
-	"datePublished": "<?php echo get_the_date('d.m.Y'); ?>",
-	"dateModified": "<?php echo the_modified_date('d.m.Y'); ?>"
-	}
-	</script>
-	<?php endif; ?>
+	<?php /* if (is_singular('post') && function_exists('tsf')) :
+		get_template_part( 'template-parts/schema/schema.php', 'post' ); // needs further edits on the file itself
+	endif; */ ?>
 
 	<!-- Zapis schema dla strony glownej - typ Localbusiness / Schema for homepage - LocalBusiness -->
-	<?php /* if (is_page_template('template-kontakt.php') && function_exists('tsf')) ?>
-	<script type="application/ld+json">
-	{
-	"@context": "https://schema.org",
-	"@type": "Attorney",
-	"name": "Business name",
-	"image": "https://url-image.com",
-	"@id": "",
-	"url": "https://website-url.com",
-	"telephone": "+48111222333",
-	"address": {
-		"@type": "PostalAddress",
-		"streetAddress": "example street 1/2",
-		"addressLocality": "City",
-		"postalCode": "11-222",
-		"addressCountry": "PL"
-	},
-	"geo": {
-		"@type": "GeoCoordinates",
-		"latitude": 1,
-		"longitude": 2
-	} ,
-	"sameAs": [
-		"https://www.facebook.com/",
-		"https://www.instagram.com/",
-		"https://www.youtube.com/",
-		"https://www.linkedin.com/"
-	] 
-	}
-	</script>
+	<?php /* if (is_page_template('template-kontakt.php') && function_exists('tsf')) : ?>
+		get_template_part( 'template-parts/schema/schema.php', 'business' ); // needs further edits on the file itself
 	<?php endif; */ ?>
 
 </head>
