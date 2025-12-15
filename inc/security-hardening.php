416ea2bb96c6005eb4565d422e704c2cb9a86577
Nikodemsky <Nikodemh@gmail.com>
2025-12-15 14:11:29 +0100

    Create security-hardening.php

---
+++ inc/security-hardening.php
@@ -0,0 +1,29 @@
+<?php
+
+// Removes WP version info from rss
+remove_action('wp_head', 'wp_generator');
+
+function my_secure_generator( $generator, $type ) {
+	return '';
+}
+add_filter( 'the_generator', 'my_secure_generator', 10, 2 );
+
+function my_remove_src_version( $src ) {
+	global $wp_version;
+
+	$version_str = '?ver='.$wp_version;
+	$offset = strlen( $src ) - strlen( $version_str );
+
+	if ( $offset >= 0 && strpos($src, $version_str, $offset) !== FALSE )
+		return substr( $src, 0, $offset );
+
+	return $src;
+}
+add_filter( 'script_loader_src', 'my_remove_src_version' );
+add_filter( 'style_loader_src', 'my_remove_src_version' );
+
+// Disable Application passwords
+add_filter('wp_is_application_passwords_available', '__return_false');
+
+// Disable XMLRPC
+add_filter('xmlrpc_enabled', '__return_false');
