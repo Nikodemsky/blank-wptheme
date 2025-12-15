6c97d77164ae66de7066625f6ff5f9298c46ac5e
Nikodemsky <Nikodemh@gmail.com>
2025-12-15 14:29:49 +0100

    Supports directives moved to external file

--- functions.php
+++ functions.php
@@ -7,15 +7,7 @@ if(!defined('_S_VERSION')){define('_S_VERSION','1.0.0');}
 require get_template_directory() . '/inc/custom-header.php';
 require get_template_directory() . '/inc/template-tags.php';
 require get_template_directory() . '/inc/customizer.php';
-
-if(!function_exists('wgblank_setup')):function wgblank_setup(){load_theme_textdomain('wg-blank',get_template_directory().'/languages');add_theme_support('automatic-feed-links');add_theme_support('title-tag');add_theme_support('post-thumbnails');register_nav_menus(array('menu-1'=>esc_html__('Main menu','wg-blank'),));add_theme_support('html5',array('search-form','comment-form','comment-list','gallery','caption','style','script',));add_theme_support('custom-background',apply_filters('wgblank_custom_background_args',array('default-color'=>'ffffff','default-image'=>'',)));add_theme_support('customize-selective-refresh-widgets');add_theme_support('custom-logo',array('height'=>250,'width'=>250,'flex-width'=>true,'flex-height'=>true,));}
-endif;add_action('after_setup_theme','wgblank_setup');
-
-function wgblank_content_width(){$GLOBALS['content_width'] = apply_filters( 'wgblank_content_width', 1025 );}
-add_action( 'after_setup_theme', 'wgblank_content_width', 0 );
-
-function wgblank_scripts(){wp_enqueue_style('wgblank-style',get_stylesheet_uri(),array(),_S_VERSION);wp_style_add_data('wgblank-style','rtl','replace');}
-add_action('wp_enqueue_scripts','wgblank_scripts');
+require get_template_directory() . '/inc/theme-support.php';
 
 /*********** SECURITY - HARDENING ***********/
 
@@ -207,6 +199,7 @@ add_action('wp_trash_post', 'clear_post_type_globals_cache');
 add_action('untrash_post', 'clear_post_type_globals_cache');
 
 add_action('init', 'set_post_type_globals');*/
+
 
 
 
