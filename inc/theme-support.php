adeb4814b77966ba763aac14e0b9f8633aae5944
Nikodemsky <Nikodemh@gmail.com>
2025-12-20 05:22:12 +0100

    site editor removal

--- inc/theme-support.php
+++ inc/theme-support.php
@@ -10,7 +10,6 @@ if (!function_exists("wgblank_setup")):
         );
         //add_theme_support("automatic-feed-links");
         add_theme_support("title-tag");
-        add_theme_support("dark-editor-style");
         add_theme_support("post-thumbnails");
         register_nav_menus(["menu-1" => esc_html__("Main menu", "wg-blank")]);
         add_theme_support("html5", [
