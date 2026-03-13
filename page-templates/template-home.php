876b8e46948749212d698d737af44a7631eeae28
Nikodemsky <Nikodemh@gmail.com>
2026-03-13 01:29:49 +0100

    filled example

--- page-templates/template-home.php
+++ page-templates/template-home.php
@@ -1,1 +1,20 @@
 
+<?php
+
+/* Template name: Home */
+
+// Header
+get_header();
+
+?>
+
+<main id="primary" class="site-main">
+
+    <?php 
+    //get_template_part('template-parts/home/section', 'claim'); 
+    ?>
+
+</main>
+
+<?php
+get_footer();
